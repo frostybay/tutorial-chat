5210798f77a77b16e487749f7dda4931fc93ad99
carloscadori <carloscadori.dev@gmail.com>
2016-08-16 18:12:34 -0300

    chat: sending and receiving message in the same script

---
+++ Assets/Scripts/Chat.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+public class Chat : MonoBehaviour
+{
+	public const short chatMessage = 131;
+
+	[SerializeField]
+	private Text container;
+
+	private void Start()
+	{
+		if (NetworkServer.active) 
+		{
+			NetworkServer.RegisterHandler(chatMessage, ServerReceiveMessage);
+		}
+		
+		NetworkManager.singleton.client.RegisterHandler (chatMessage, ReceiveMessage);
+	}
+
+	private void ServerReceiveMessage(NetworkMessage message)
+	{
+		StringMessage myMessage = new StringMessage ();
+		myMessage.value = message.conn.connectionId + ": " + message.ReadMessage<StringMessage> ().value;
+
+		NetworkServer.SendToAll (Chat.chatMessage, myMessage);
+	}
+
+	private void ReceiveMessage(NetworkMessage message)
+	{
+		string messageText = message.ReadMessage<StringMessage> ().value;
+		container.text += "\n" + messageText;
+	}
+
+	public void SendMessage(InputField input)
+	{
+		StringMessage myMessage = new StringMessage ();
+		myMessage.value = input.text;
+
+		NetworkManager.singleton.client.Send (chatMessage, myMessage);
+	}
+}
