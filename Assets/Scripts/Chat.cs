a8702a235ff1c4b90907a0940c5efc23cb755a49
carloscadori <carloscadori.dev@gmail.com>
2016-08-16 19:11:07 -0300

    some comments

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking.NetworkSystem;
 
 public class Chat : MonoBehaviour
 {
+	//just a random number
 	public const short chatMessage = 131;
 
 	[SerializeField]
@@ -12,33 +13,42 @@ public class Chat : MonoBehaviour
 
 	private void Start()
 	{
+		//if the client is also the server
 		if (NetworkServer.active) 
 		{
+			//registering the server handler
 			NetworkServer.RegisterHandler(chatMessage, ServerReceiveMessage);
 		}
 		
+		//registering the client handler
 		NetworkManager.singleton.client.RegisterHandler (chatMessage, ReceiveMessage);
 	}
 
 	private void ServerReceiveMessage(NetworkMessage message)
 	{
 		StringMessage myMessage = new StringMessage ();
+		//we are using the connectionId as player name only to exemplify
 		myMessage.value = message.conn.connectionId + ": " + message.ReadMessage<StringMessage> ().value;
 
+		//sending to all connected clients
 		NetworkServer.SendToAll (Chat.chatMessage, myMessage);
 	}
 
 	private void ReceiveMessage(NetworkMessage message)
 	{
+		//reading message
 		string messageText = message.ReadMessage<StringMessage> ().value;
+		//adding message to chat
 		container.text += "\n" + messageText;
 	}
 
 	public void SendMessage(InputField input)
 	{
 		StringMessage myMessage = new StringMessage ();
+		//getting the value of the input
 		myMessage.value = input.text;
 
+		//sending to server
 		NetworkManager.singleton.client.Send (chatMessage, myMessage);
 	}
 }
