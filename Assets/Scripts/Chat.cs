eacea63fde0dd48b4467be361bebbf4bbcc1fff4
carloscadori <carloscadori.dev@gmail.com>
2016-08-17 19:44:07 -0300

    separating interface in UNET script

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -1,62 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Networking;
-using UnityEngine.Networking.NetworkSystem;
-using System.Collections;
 
 public class Chat : MonoBehaviour
 {
-	//just a random number
-	public const short chatMessage = 131;
-
 	[SerializeField]
 	private Text container;
 	[SerializeField]
 	private ScrollRect rect;
 
-	private void Start()
+	internal void AddMessage(string message)
 	{
-		//if the client is also the server
-		if (NetworkServer.active) 
-		{
-			//registering the server handler
-			NetworkServer.RegisterHandler(chatMessage, ServerReceiveMessage);
-		}
-		
-		//registering the client handler
-		NetworkManager.singleton.client.RegisterHandler (chatMessage, ReceiveMessage);
-	}
-
-	private void ServerReceiveMessage(NetworkMessage message)
-	{
-		StringMessage myMessage = new StringMessage ();
-		//we are using the connectionId as player name only to exemplify
-		myMessage.value = message.conn.connectionId + ": " + message.ReadMessage<StringMessage> ().value;
-
-		//sending to all connected clients
-		NetworkServer.SendToAll (Chat.chatMessage, myMessage);
-	}
-
-	private void ReceiveMessage(NetworkMessage message)
-	{
-		//reading message
-		string messageText = message.ReadMessage<StringMessage> ().value;
-		//adding message to chat
-		container.text += "\n" + messageText;
+		container.text += "\n" + message;
 
 		//just a hack to jump a frame and scrolldown the chat
 		Invoke("ScrollDown", .1f);
 	}
 
-	public void SendMessage(InputField input)
-	{
-		StringMessage myMessage = new StringMessage ();
-		//getting the value of the input
-		myMessage.value = input.text;
-
-		//sending to server
-		NetworkManager.singleton.client.Send (chatMessage, myMessage);
-	}
+	public virtual void SendMessage(InputField input)	{}
 
 	private void ScrollDown()
 	{
---
+++ Assets/Scripts/UNETChat.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+public class UNETChat : Chat
+{
+	//just a random number
+	public const short chatMessage = 131;
+
+	private void Start()
+	{
+		//if the client is also the server
+		if (NetworkServer.active) 
+		{
+			//registering the server handler
+			NetworkServer.RegisterHandler(chatMessage, ServerReceiveMessage);
+		}
+
+		//registering the client handler
+		NetworkManager.singleton.client.RegisterHandler (chatMessage, ReceiveMessage);
+	}
+
+	private void ReceiveMessage(NetworkMessage message)
+	{
+		//reading message
+		string text = message.ReadMessage<StringMessage> ().value;
+
+		AddMessage (text);
+	}
+
+	private void ServerReceiveMessage(NetworkMessage message)
+	{
+		StringMessage myMessage = new StringMessage ();
+		//we are using the connectionId as player name only to exemplify
+		myMessage.value = message.conn.connectionId + ": " + message.ReadMessage<StringMessage> ().value;
+
+		//sending to all connected clients
+		NetworkServer.SendToAll (chatMessage, myMessage);
+	}
+
+	public override void SendMessage (UnityEngine.UI.InputField input)
+	{
+		StringMessage myMessage = new StringMessage ();
+		//getting the value of the input
+		myMessage.value = input.text;
+
+		//sending to server
+		NetworkManager.singleton.client.Send (chatMessage, myMessage);
+	}
+}
